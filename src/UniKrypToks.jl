fa3c59f22743f837544cd37f950eb14e9f4a8eda
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2025-05-05 12:01:29 -0300

    UPD the src/UniKrypToks.jl

--- src/UniKrypToks.jl
+++ src/UniKrypToks.jl
@@ -1,6 +1,25 @@
-# Module
-module UniCrypToks
+#--------------------------------------------------------------------------------------------------#
+#                                          UniKrypToks.jl                                          #
+#--------------------------------------------------------------------------------------------------#
 
+# module
+module UniKrypToks
 
-# Module
+# imports
+using Reexport
+@reexport using FixedPointDecimal
+@reexport using Printf
+
+# include - abstract supertypes
+include("abstract.jl")
+
+# include - constants
+include("constant.jl")
+
+# include - token amount
+include("tokenAmt.jl")
+
+# module
 end
+
+
