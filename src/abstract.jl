7ca900867de7964ac699e96d7baf88b0655579fc
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2025-05-05 12:22:40 -0300

    ADD the src/abstract.jl

---
+++ src/abstract.jl
@@ -0,0 +1,39 @@
+#--------------------------------------------------------------------------------------------------#
+#                                           abstract.jl                                            #
+#--------------------------------------------------------------------------------------------------#
+
+#--------------------------------------------------------------------------------------------------#
+#                             Top-Level AbstractFinance abstract type                              #
+#--------------------------------------------------------------------------------------------------#
+
+"""
+`abstract type AbstractFinance <: Any end`\n
+Top-Level AbstractFinance abstract type, under which every custom AbstractFinance type must be
+hooked into.
+"""
+abstract type AbstractFinance <: Any end
+
+# export
+export AbstractFinance
+
+
+#--------------------------------------------------------------------------------------------------#
+#                                    AbstractTokenAmount branch                                    #
+#--------------------------------------------------------------------------------------------------#
+
+"""
+`abstract type AbstractTokenAmount <: AbstractFinance end`\n
+Abstract type for all token amount types.
+"""
+abstract type AbstractTokenAmount <: AbstractFinance end
+
+"""
+`abstract type PlainAmount <: AbstractTokenAmount end`\n
+Abstract type for all plain (uniform-precision) amount types.
+"""
+abstract type PlainAmount <: AbstractTokenAmount end
+
+# export
+export AbstractTokenAmount, PlainAmount
+
+
