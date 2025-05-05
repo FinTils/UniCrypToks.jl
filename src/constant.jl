a0044179a4c8708b434db12fefc46a47c5a4942e
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2025-05-05 12:22:40 -0300

    ADD the src/constant.jl

---
+++ src/constant.jl
@@ -0,0 +1,21 @@
+#--------------------------------------------------------------------------------------------------#
+#                                           constant.jl                                            #
+#--------------------------------------------------------------------------------------------------#
+
+"""
+`const DEF`\n
+Constant definitions for the package.
+"""
+const DEF = (decimals = 10,
+             FixDecTy = Int128,)
+
+# Resulting FixedDecimal type
+const FD = FixedDecimal{DEF.FixDecTy, DEF.decimals}
+
+# Resulting denominator
+const DENO = DEF.FixDecTy(10)^DEF.decimals
+
+# export
+export DEF
+
+
