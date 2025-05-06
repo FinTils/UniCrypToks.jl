2a0c2f09461b9b9486df83f296ab384d2e265af2
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2025-05-06 20:28:52 -0300

    UPD the src/fiatAmt.jl

--- src/fiatAmt.jl
+++ src/fiatAmt.jl
@@ -2,6 +2,10 @@
 #                                            fiatAmt.jl                                            #
 #--------------------------------------------------------------------------------------------------#
 
+isFiat(x::Symbol) = x in Currencies.allsymbols()
+isCryp(x::Symbol) = !isFiat(x)
+
+
 #--------------------------------------------------------------------------------------------------#
 #                                      fiatAmt <: toknAmount                                       #
 #--------------------------------------------------------------------------------------------------#
@@ -14,10 +18,18 @@ struct fiatAmt <: toknAmount
     sym::Symbol
     val::FD
     function fiatAmt(sym::Symbol, val::FD)
-        ## @assert()
-        new(sym, val)
+        @assert(isFiat(sym), @sprintf("%s is not a fiat currency!", sym))
+        SYM = string(sym)
+        if length(SYM) > 6
+            eid = collect(eachindex(SYM))
+            SYM = SIM[eid[1]:eid[6]]
+        end
+        new(Symbol(SYM), val)
     end
 end
+
+# outer constructors
+fiatAmt(sym::Symbol, val::Real) = fiatAmt(sym, FD(val))
 
 # export
 export fiatAmt
