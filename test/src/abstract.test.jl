3c38e96c8ceb60904a4092729980163ebf80fcf7
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2025-05-06 12:48:07 -0300

    UPD the test/src/abstract.test.jl

--- test/src/abstract.test.jl
+++ test/src/abstract.test.jl
@@ -3,15 +3,17 @@
 #--------------------------------------------------------------------------------------------------#
 
 @testset "abstract.test.jl                                                " begin
-    # --- Down-walking abstract type tests
-    𝑝 = Any
-    for 𝑠 in [:AbstractFinance, :AbstractTokenAmount, :PlainAmount]
+    # --- Generalized abstract type tests
+    for 𝑝, 𝑠 in [(:Any                      ,  :AbstractFinance        ),
+                 (  :AbstractFinance        ,    :AbstractTokenAmount  ),
+                 (    :AbstractTokenAmount  ,      :bareAmount         ),
+                 (    :AbstractTokenAmount  ,      :toknAmount         ),
+                ]
         @test isdefined(FinTilsBase, 𝑠)         # Is it defined in the module?
         @test isdefined(Main, 𝑠)                # Is is exported?
         @test hasdoc(FinTilsBase, 𝑠)            # Is it documented?
-        @test eval(𝑠) isa DataType              # Is it a DataType
-        @test eval(𝑠) in subtypes(eval(𝑝))
-        𝑝 = 𝑠
+        @test eval(𝑠) isa DataType              # Is it a DataType?
+        @test eval(𝑠) in subtypes(eval(𝑝))      # Is 𝑠 a direct subtype of 𝑝?
     end
 end
 
