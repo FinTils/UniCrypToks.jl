dc55dcb16b41c4eda70d55e7b471e058bd0392c0
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2025-05-06 21:36:08 -0300

    UPD the src/abstract.jl

--- src/abstract.jl
+++ src/abstract.jl
@@ -22,18 +22,27 @@ export AbstractFinance
 #--------------------------------------------------------------------------------------------------#
 
 """
-`abstract type AbstractTokenAmount <: AbstractFinance end`\n
+`abstract type UniformAmount <: AbstractFinance end`\n
 Abstract type for all token amount types.
 """
-abstract type AbstractTokenAmount <: AbstractFinance end
+abstract type UniformAmount <: AbstractFinance end
 
 """
-`abstract type toknAmount <: AbstractTokenAmount end`\n
+`abstract type toknAmount <: UniformAmount end`\n
 Abstract type for all (uniform-precision) currency amount types.
 """
-abstract type toknAmount <: AbstractTokenAmount end
+abstract type toknAmount <: UniformAmount end
+
+"""
+`abstract type pairAmount <: UniformAmount end`\n
+Abstract type for all (uniform-precision) paired/tracked currency amount types.
+
+A paired (or, fiat-tracked crypto) currency amount is a cryptocurrency amount paired with it's
+corresponding fiat purchase price.
+"""
+abstract type pairAmount <: UniformAmount end
 
 # export
-export AbstractTokenAmount, toknAmount
+export UniformAmount, toknAmount, pairAmount
 
 
