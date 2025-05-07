997977b4e162db4df3de528d92325a826b2434bf
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2025-05-07 01:45:38 -0300

    UPD the src/ratio.jl

--- src/ratio.jl
+++ src/ratio.jl
@@ -9,7 +9,7 @@ A unified-precision crypto token ratio.
 struct toknRatio{𝗡, 𝗗} <: UniformRatio where {𝗡 <: toknAmount, 𝗗 <: toknAmount}
     N::𝗡
     D::𝗗
-    toknRatio(n::toknAmount, d::toknAmount) = new(n, d)
+    toknRatio(n::toknAmount, d::toknAmount) = new{typeof(n), typeof(d)}(n, d)
 end
 
 (num(r::toknRatio{𝗡, 𝗗})::𝗡) where {𝗡 <: toknAmount, 𝗗 <: toknAmount} = r.N
@@ -17,19 +17,21 @@ end
 (den(r::toknRatio{𝗡, 𝗗})::𝗗) where {𝗡 <: toknAmount, 𝗗 <: toknAmount} = r.D
 
 (rat(r::toknRatio{𝗡, 𝗗})::Rational{DEF.FixDecTy}) where {𝗡 <: toknAmount, 𝗗 <: toknAmount} =
-    Rational{DEF.FixDecTy}(num(r).i, den(r).i)
+    Rational{DEF.FixDecTy}(bare(num(r)).i, bare(den(r)).i)
 
 function simplify(r::toknRatio{𝗡, 𝗗}) where {𝗡 <: toknAmount, 𝗗 <: toknAmount}
     ratio = rat(r)
-    toknRatio(one(num(r)) * ratio.num, one(den(r)) * ratio.den)
+    toknRatio(one(num(r)) / DENO * ratio.num, one(den(r)) / DENO * ratio.den)
 end
 
 # export
 export toknRatio, num, den, rat, simplify
 
 # show
-function show(io::IO, ::MIME"text/plain", r::toknRatio)
-    print(@sprintf("%s : %s", num(r), den(r)))
+function show(io::IO, r::toknRatio)
+    print(num(r))
+    print(" : ")
+    print(den(r))
 end
 
 
